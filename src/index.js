7fa9924ae137759049067bcef63176b6d4ea50ec
Adam Miskiewicz <adam@adammiskiewicz.com>
2015-11-09 13:12:20 -0500

    Catching uncaught errors, so the promise chain doesn't swallow them all up
    nom nom nom

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ export default function promiseMiddleware({ dispatch }) {
             id: sequenceId
           }
         })
-      );
+      ).catch(error => console.error(error));
     }
 
     return next(action);
